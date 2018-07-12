8f4798c227837d78a1bcd61403f6a1e5053ac13c
lxklssn <alkla2715@gmail.com>
2018-07-12 12:38:13 +0200

    fix - urls for issues

--- jiraissues-plugin/src/main/groovy/de/lxklssn/jira/GetJiraIssuesTask.groovy
+++ jiraissues-plugin/src/main/groovy/de/lxklssn/jira/GetJiraIssuesTask.groovy
@@ -26,7 +26,7 @@ class GetJiraIssuesTask extends DefaultTask {
     @TaskAction
     public void get() {
         SnippetFileCreator snippetFileCreator = new SnippetFileCreator(filePath.get());
-        IssueChapterMapper issueChapterMapper = new IssueChapterMapper(jiraBaseUrl.get() + "/browse");
+        IssueChapterMapper issueChapterMapper = new IssueChapterMapper(jiraBaseUrl.get() + "browse/");
         JiraRESTClient jiraRESTClient = new JiraRESTClient(jiraBaseUrl.get() + "/rest/api/2/" , jiraUsername.get(), jiraPassword.get(), jiraProjects.get().split(','))
         jiraVersions.get().split(',').each { jiraVersion ->
             def issuesResponse = jiraRESTClient.getIssues(jiraVersion)
